4cd0dbc92c203ee50dbff310c67effebe71742cc
Ksenia Shalimova <shalimovakss@xmail.ru>
2023-12-30 19:20:39 +0500

    seminar2_4

---
+++ seminar2_4/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = 568;
+if (n<10)
+{
+    Console.WriteLine(n);
+}
+else
+{
+    while (n>0)
+    {
+        int exit = n%10;
+        n = n/10;
+        if (n>0)
+        {
+            Console.Write(exit + ",");
+        }
+        else
+        {
+            Console.Write(exit);
+        }
+    }
+}
