9ef7254e895bba01f79f3b2e5de7fcfd63e18327
Ksenia Shalimova <shalimovakss@xmail.ru>
2023-12-30 18:58:34 +0500

    add seminar2_2

---
+++ seminar2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int x = -23;
+int y = -1;
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
